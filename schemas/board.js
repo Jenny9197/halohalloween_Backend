4f960a2d19ca89c0c10d0753900ae227205d4176
Sung Hyun Hong (Jenny) <57204918+Jenny9197@users.noreply.github.com>
2021-10-20 01:38:07 +0900

    Add files via upload

--- schemas/board.js
+++ schemas/board.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const boardSchema = new mongoose.Schema({
   postingTitle: String,
-  postingEmail: String,
   postingAuthor: String,
   postingDate: String,
   postingUpdate: String,
@@ -10,7 +9,9 @@ const boardSchema = new mongoose.Schema({
   postingImgUrl: String,
   postingVideoUrl: String,
   postingTag: String,
+  postingSeen: Number,
   postingDel: Number,
+  postingSeen: Number,
 });
 boardSchema.virtual("postID").get(function() {
   return this._id.toHexString();
